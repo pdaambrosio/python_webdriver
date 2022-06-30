e989d6dcb51b7cdd245afd1ba9bcbfd372fd347a
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-06-30 17:35:57 -0300

    test: test to access

--- hitpoint.py
+++ hitpoint.py
@@ -1,9 +1,14 @@
+from time import sleep
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 PATH = '/home/kali-user/Downloads/chromedriver'
 driver = webdriver.Chrome(PATH)
 
-driver.get('https://www.google.com/')
-driver.find_element_by_class_name('gNO89b').submit()
+driver.get('url')
+sleep(2)
+driver.find_element_by_id('button-1020').click()
+sleep(1)
+driver.find_element_by_id('ext-135').send_keys('user')
+driver.find_element_by_id('ext-137').send_keys('password')
+sleep(1)
+# driver.find_element_by_id('ext-139').click()
