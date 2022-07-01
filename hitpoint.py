bfe32a97328bb9a70c6e4536b54bedb188f80703
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-01 16:24:05 -0300

    feature: function

--- hitpoint.py
+++ hitpoint.py
@@ -1,14 +1,40 @@
-from time import sleep
-from selenium import webdriver
+def punch_the_clock(url: str, user: str, password: str) -> str:
+    from time import sleep
+    from selenium import webdriver
+    from datetime import datetime
+    from typing import Any
 
-PATH = '/home/kali-user/Downloads/chromedriver'
-driver = webdriver.Chrome(PATH)
+    page_title: str
+    page_result: Any
 
-driver.get('url')
-sleep(2)
-driver.find_element_by_id('button-1020').click()
-sleep(1)
-driver.find_element_by_id('ext-135').send_keys('user')
-driver.find_element_by_id('ext-137').send_keys('password')
-sleep(1)
-# driver.find_element_by_id('ext-139').click()
+    options: webdriver = webdriver.ChromeOptions()
+    options.headless = True
+    path: str = './chromedriver'
+    driver: webdriver = webdriver.Chrome(path, options=options)
+
+    driver.get(url)
+    sleep(2)
+    driver.find_element_by_id('button-1020').click()
+    sleep(1)
+    driver.find_element_by_id('ext-135').send_keys(user)
+    driver.find_element_by_id('ext-137').send_keys(password)
+    sleep(1)
+    driver.find_element_by_id('ext-139').click()
+    page_title = driver.title
+    page_result = driver.find_element_by_id('ext-141').text
+    date: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    sleep(2)
+    driver.close()
+    return f'{page_title}\n{page_result}: {date}'
+
+
+def main():
+    url: str = 'https://test.com'
+    user: str = 'admin'
+    password: str = '123456'
+    print(punch_the_clock(url, user, password))
+
+
+if '__main__' == __name__:
+    main()
+    exit(0)
