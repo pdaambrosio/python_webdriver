209f72e46e1318a6cf0551a8f89763dc0b911159
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-01 13:25:06 -0300

    feature: datetime

--- hitpoint.py
+++ hitpoint.py
@@ -1,14 +1,24 @@
 from time import sleep
 from selenium import webdriver
+from datetime import datetime
 
-PATH = '/home/kali-user/Downloads/chromedriver'
-driver = webdriver.Chrome(PATH)
+options: webdriver = webdriver.ChromeOptions()
+options.headless = True
 
-driver.get('url')
+path: str = '/home/kali-user/Downloads/chromedriver'
+driver: webdriver = webdriver.Chrome(path, options=options)
+
+driver.get('URL')
 sleep(2)
 driver.find_element_by_id('button-1020').click()
 sleep(1)
-driver.find_element_by_id('ext-135').send_keys('user')
-driver.find_element_by_id('ext-137').send_keys('password')
+driver.find_element_by_id('ext-135').send_keys('USER')
+driver.find_element_by_id('ext-137').send_keys('PASSWORD')
 sleep(1)
-# driver.find_element_by_id('ext-139').click()
+driver.find_element_by_id('ext-139').click()
+print(driver.find_element_by_id('ext-141').text, '-', datetime.now())
+print(driver.title)
+sleep(2)
+driver.close()
+
+
