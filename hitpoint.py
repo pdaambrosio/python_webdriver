68fdd97a4a085d386a8795317f53b9a2e97948c7
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-04 13:20:20 -0300

    fix: function and included requirements.txt

--- hitpoint.py
+++ hitpoint.py
@@ -1,10 +1,9 @@
 def punch_the_clock(url: str, user: str, password: str) -> str:
+    from datetime import datetime
     from time import sleep
     from selenium import webdriver
-    from datetime import datetime
     from typing import Any
 
-    page_title: str
     page_result: Any
 
     options: webdriver = webdriver.ChromeOptions()
@@ -20,12 +19,11 @@ def punch_the_clock(url: str, user: str, password: str) -> str:
     driver.find_element_by_id('ext-137').send_keys(password)
     sleep(1)
     driver.find_element_by_id('ext-139').click()
-    page_title = driver.title
     page_result = driver.find_element_by_id('ext-141').text
     date: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     sleep(2)
     driver.close()
-    return f'{page_title}\n{page_result} - {date}'
+    return f'{page_result} - {date}'
 
 
 def main():
@@ -33,8 +31,8 @@ def main():
         url: str = 'https://www.hitpoint.co.jp/'
         user: str = 'admin'
         password: str = 'admin'
-        with open('logs.txt', 'w') as logs:
-            logs.write(punch_the_clock(url, user, password))
+        with open('logs.txt', 'a+') as logs:
+            logs.write(punch_the_clock(url, user, password) + '\n')
     except KeyboardInterrupt:
         print('\nBye!')
 
