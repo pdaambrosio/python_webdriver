8f8b494f86bfc4f139efba6da963f8d0c506a8f2
pdaambrosio <pda.ambrosio@gmail.com>
2023-09-06 06:56:27 -0300

    refact: webdriver 2.0

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+#!/home/kali-user/courses_projects/scripts/python_envs/web_hit_point/bin/python3
 from models.web_hit import punch_the_clock
+import pathlib
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 def main():
+    """
+    The main function attempts to punch the clock on a website using the provided URL, user, and
+    password, and logs the result in an access.logs file. If an error occurs, it logs the error in an
+    error.logs file.
+    """
     try:
-        url: str = 'https://test.com/'
-        user: str = 'admin'
-        password: str = 'admin'
-        with open('logs.txt', 'a+') as logs:
-            logs.write(punch_the_clock(url, user, password) + '\n')
-    except Exception as error:
-        print(error)
+        url: str = 'https://cliente.apdata.com.br/everisparceiro/'
+        user: str = os.getenv('WEB_USER')
+        password: str = os.getenv('WEB_PASSWORD')
+        with open(pathlib.Path().resolve()/'access.logs', 'a+') as access:
+            access.write(punch_the_clock(url, user, password) + '\n')
+    except Exception as err:
+        with open(pathlib.Path().resolve()/'error.logs', 'a+') as error:
+            error.write(f'{err}\n')
 
 
 if '__main__' == __name__:
--- models/web_hit.py
+++ models/web_hit.py
@@ -2,18 +2,37 @@ from datetime import datetime
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from typing import Any
 
 
 def punch_the_clock(url: str, user: str, password: str) -> str:
+    """
+    The function `punch_the_clock` logs into a website using the provided URL, username, and password,
+    performs some actions, retrieves a page result, and returns the result along with the current date
+    and time.
+    
+    :param url: The `url` parameter is a string that represents the URL of the webpage where the clock
+    punching functionality is located
+    :type url: str
+    :param user: The `user` parameter is a string that represents the username or user ID used to log in
+    to the website or application
+    :type user: str
+    :param password: The `password` parameter is a string that represents the password for the user to
+    log in to the website specified by the `url` parameter
+    :type password: str
+    :return: a string that consists of the page result and the current date and time.
+    """
     page_result: Any
 
     options: webdriver = webdriver.ChromeOptions()
-    options.headless = True
-    path: str = 'chromedriver'
-    driver: webdriver = webdriver.Chrome(path, options=options)
-    # driver: webdriver = webdriver.Chrome(path, options=None)
-
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    service_path: Service = Service(ChromeDriverManager().install())
+    driver: webdriver = webdriver.Chrome(service=service_path, options=options)
+    
     driver.get(url)
     sleep(3)
     driver.find_element(By.ID, 'button-1020').click()
@@ -24,7 +43,7 @@ def punch_the_clock(url: str, user: str, password: str) -> str:
     driver.find_element(By.ID, 'ext-139').click()
     sleep(1)
     page_result = driver.find_element(By.ID, 'ext-141').text
-    date: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    date: str = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     sleep(2)
     driver.close()
     return f'{page_result} - {date}'
